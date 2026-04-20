f3488138c8a76e1a45381432b9b572209f4bda41
anna.bielskaya <anna.bielskaya@gmail.com>
2026-04-21 00:13:01 +0300

    Added reusable button component & NoProjectSelected component

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./common/Button";
 
 const ProjectsSidebar = () => {
   return (
@@ -7,14 +8,7 @@ const ProjectsSidebar = () => {
         Your Projects
       </h2>
       <div>
-        <button
-          onClick={() => {
-            console.log("Henlo");
-          }}
-          className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-        >
-          + Add Project
-        </button>
+        <Button title="+ Add Project" />
       </div>
       <ul>
         <p>List of projects</p>
